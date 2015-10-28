29c355895c0342ceac874dc29e0b726c11372be0
will-09 <william_lee09@hotmail.com>
2015-10-28 12:41:54 -0700

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -25,4 +25,9 @@ ActiveRecord::Schema.define(version: 20151016090730) do
     t.datetime "updated_at",             null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
