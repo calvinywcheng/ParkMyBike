36431c6c7f6c89ff065e5a74fe87fc42a4adc215
will-09 <william_lee09@hotmail.com>
2015-11-24 08:29:44 -0800

    database update

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
-Rails.application.config.middleware.use OmniAuth::Builder do 
+Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :facebook, "706274382806077", "0ebecdc770dc8f7e6c04c266ec2afc23", scope: "publish_actions"
 end
